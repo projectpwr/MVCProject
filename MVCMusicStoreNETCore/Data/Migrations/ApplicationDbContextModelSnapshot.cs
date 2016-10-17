139f98f8591b2367b11e46ac024aedeb641eb212
Will Young <willyoung0@googlemail.com>
2016-10-17 22:09:53 +0100

    something VS looks to have done in regards to db migratons

--- MVCMusicStoreNETCore/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MVCMusicStoreNETCore/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using MVCMusicStoreNETCore.Data;
 
 namespace MVCMusicStoreNETCore.Data.Migrations
 {
@@ -15,7 +13,7 @@ namespace MVCMusicStoreNETCore.Data.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
             modelBuilder
-                .HasAnnotation("ProductVersion", "1.0.0-rc3")
+                .HasAnnotation("ProductVersion", "1.0.1")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole", b =>
